75ddc07cac382efd0f6ea0e5986f95c77f414090
Rafael Ramos <rafaelh@inmotionhosting.com>
2016-09-07 10:18:42 -0400

    Updating Versions

--- boldgrid-editor.php
+++ boldgrid-editor.php
@@ -3,7 +3,7 @@
  * Plugin Name: BoldGrid Editor
  * Plugin URI: http://www.boldgrid.com
  * Description: Customized editing for pages and posts
- * Version: 1.2.4
+ * Version: 1.2.5
  * Author: BoldGrid.com <wpb@boldgrid.com>
  * Author URI: http://www.boldgrid.com
  * Text Domain: boldgrid-editor
@@ -18,7 +18,7 @@ if ( false === defined( 'WPINC' ) ) {
 
 // Define Editor version.
 if ( false === defined( 'BOLDGRID_EDITOR_VERSION' ) ) {
-	define( 'BOLDGRID_EDITOR_VERSION', '1.2.4' );
+	define( 'BOLDGRID_EDITOR_VERSION', '1.2.5' );
 }
 
 // Define Editor path.
