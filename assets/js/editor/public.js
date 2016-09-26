d8f092976bd6f42fc75d7d989a256fd0415869c1
Joe C <joe@css-solutions.com>
2016-09-26 11:24:52 -0400

    WPB-2420: Added EOF line breaks.

--- assets/js/editor/public.js
+++ assets/js/editor/public.js
@@ -1,12 +1,12 @@
 ( function ( $ ) {
-	
+
 	$( function () {
 		onload();
 	} );
-	
+
 	function onload() {
 		$('.background-parallax').attr( 'data-stellar-background-ratio', '.3' );
 		$('body').stellar();
 	};
-	
+
 } )( jQuery );
