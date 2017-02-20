ca94f4af4ff6eb20ece3293d7c62e6e7580d61a7
BradM <bradm@inmotionhosting.com>
2017-02-20 09:59:05 -0500

    Updating version numbers

--- boldgrid-editor.php
+++ boldgrid-editor.php
@@ -3,7 +3,7 @@
  * Plugin Name: BoldGrid Editor
  * Plugin URI: http://www.boldgrid.com
  * Description: Customized editing for pages and posts
- * Version: 1.3.4
+ * Version: 1.3.5
  * Author: BoldGrid.com <support@boldgrid.com>
  * Author URI: http://www.boldgrid.com
  * Text Domain: boldgrid-editor
