ea7b9d29de3f20067b9a6999c44243705910d095
Rafael Ramos <rramo012@gmail.com>
2017-10-02 13:22:43 -0400

    Build

--- assets/js/builder/gridblock/lead.js
+++ assets/js/builder/gridblock/lead.js
@@ -6,16 +6,75 @@ import template from '../../../../includes/template/gridblock/lead.html';
 export class Lead {
 
 	constructor() {
+		this.$template = $();
 	}
 
+	/**
+	 * Initialize the GridBlock Lead.
+	 *
+	 * @since 1.6
+	 */
 	init() {
-		console.log( tinymce.activeEditor.iframeElement );
 		if ( BoldgridEditor.display_gridblock_lead ) {
-			let $iframe = $( tinymce.activeEditor.iframeElement ),
-				$template = $( template );
-
-			console.log( $iframe.after( $template ) );
+			this.$template = this._insert();
+			this._bind();
 		}
+	}
+
+	/**
+	 * Dismiss the prompt for adding a GridBlock.
+	 *
+	 * @since 1.6
+	 */
+	dismissPrompt() {
+		this.$template.hide();
+	}
+
+	/**
+	 * Bind event listeners.
+	 *
+	 * @since 1.6
+	 */
+	_bind() {
+		this._bindDismiss();
+		this._bindAddGridblock();
+	}
+
+	/**
+	 * Dismiss the prompt.
+	 *
+	 * @since 1.6
+	 */
+	_bindDismiss() {
+		this.$template.find( '.add-blank' ).add( '#insert-media-button, #insert-gridblocks-button' ).one( 'click', () => {
+			this.dismissPrompt();
+		} );
+	}
+
+	/**
+	 * Add GridBlock will open the GridBlock Area.
+	 *
+	 * @since 1.6
+	 */
+	_bindAddGridblock() {
+		this.$template.find( '.add-gridblock' ).one( 'click', () => {
+			BG.CONTROLS.Section.enableSectionDrag();
+			this.dismissPrompt();
+		} );
+	}
+
+	/**
+	 * Insert the template into the page.
+	 *
+	 * @since 1.6
+	 */
+	_insert() {
+		let $iframe = $( tinymce.activeEditor.iframeElement ),
+			$template = $( template );
+
+		$iframe.after( $template );
+
+		return $template;
 	}
 
 }
