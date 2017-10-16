0f81c280516cf1814e52621b2d9ae37617d6d382
Rafael Ramos <rramo012@gmail.com>
2017-10-16 11:06:47 -0400

    Updating bg components

--- includes/template/page/entry-footer.php
+++ includes/template/page/entry-footer.php
@@ -1,6 +1,18 @@
-<?php if ( ! is_customize_preview() ) { ?>
+<?php
+
+$title = 'Edit Post';
+$post_type = get_post_type();
+if ( $post_type ) {
+	$post_type_object = get_post_type_object( $post_type );
+
+	if ( ! empty( $post_type_object->labels->singular_name ) ) {
+		$title = 'Edit ' . $post_type_object->labels->singular_name;
+	}
+}
+
+if ( ! is_customize_preview() ) { ?>
 <div class="bg-edit-link">
-	<a title="Edit Page" href="<?php print get_edit_post_link() ?>"><i class="fa fa-pencil"
+	<a title="<?php print $title ?>" href="<?php print get_edit_post_link() ?>"><i class="fa fa-pencil"
 		aria-hidden="true"></i></a>
 </div>
 <?php } ?>
